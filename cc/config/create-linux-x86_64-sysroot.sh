705b6583ba499f3846e8cdbfa786fbffc4b4398e
Yurii T. <ytopin@griddynamics.com>
2026-01-20 18:19:04 +0000

    Add GPU test with pybind...

--- cc/config/create-linux-x86_64-sysroot.sh
+++ cc/config/create-linux-x86_64-sysroot.sh
@@ -56,20 +56,20 @@ docker cp $CONTAINER:/lib /tmp/$ARCH_NAME/
 docker cp $CONTAINER:/lib64 /tmp/$ARCH_NAME/
 docker cp $CONTAINER:/usr /tmp/$ARCH_NAME/
 # Remove not used directories
-rm -rf /tmp/$ARCH_NAME/lib/cpp
-rm -rf /tmp/$ARCH_NAME/usr/bin
-rm -rf /tmp/$ARCH_NAME/usr/games
-rm -rf /tmp/$ARCH_NAME/usr/sbin
-rm -rf /tmp/$ARCH_NAME/usr/src
-rm -rf /tmp/$ARCH_NAME/usr/lib/gold-ld/ld
-rm -rf /tmp/$ARCH_NAME/usr/lib/compat-ld/ld
-rm -rf /tmp/$ARCH_NAME/usr/lib/bfd-plugins/liblto_plugin.so
+#rm -rf /tmp/$ARCH_NAME/lib/cpp
+#rm -rf /tmp/$ARCH_NAME/usr/bin
+#rm -rf /tmp/$ARCH_NAME/usr/games
+#rm -rf /tmp/$ARCH_NAME/usr/sbin
+#rm -rf /tmp/$ARCH_NAME/usr/src
+#rm -rf /tmp/$ARCH_NAME/usr/lib/gold-ld/ld
+#rm -rf /tmp/$ARCH_NAME/usr/lib/compat-ld/ld
+#rm -rf /tmp/$ARCH_NAME/usr/lib/bfd-plugins/liblto_plugin.so
 
 # Remove share directory but restore copyright files
-rm -rf /tmp/$ARCH_NAME/usr/share
-mkdir /tmp/$ARCH_NAME/usr/share
-docker cp $CONTAINER:/usr/share/common-licenses /tmp/$ARCH_NAME/usr/share/
-docker cp $CONTAINER:/usr/share/doc /tmp/$ARCH_NAME/usr/share/
+#rm -rf /tmp/$ARCH_NAME/usr/share
+#mkdir /tmp/$ARCH_NAME/usr/share
+#docker cp $CONTAINER:/usr/share/common-licenses /tmp/$ARCH_NAME/usr/share/
+#docker cp $CONTAINER:/usr/share/doc /tmp/$ARCH_NAME/usr/share/
 
 echo -e "\nCreating /tmp/$ARCH_NAME.tar.xz archive..."
 XZ_OPT="-T8"
