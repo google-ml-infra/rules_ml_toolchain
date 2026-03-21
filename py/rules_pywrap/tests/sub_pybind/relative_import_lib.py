beb7292cc67b00ff5393fb3a2107e56da9de90ba
Yurii T. <ytopin@griddynamics.com>
2026-03-21 05:34:40 +0000

    Add draft version of python tests

--- py/rules_pywrap/tests/sub_pybind/relative_import_lib.py
+++
@@ -1,8 +0,0 @@
-from .nested_pybind import nested_pybind_func
-from ..tests.sub._sub_private import _sub_private_private_func
-
-def call_nested_pybind_func(x):
-    return nested_pybind_func(x)
-
-def sub_sub_private_func(x):
-    return _sub_private_private_func(x)
--- py/rules_pywrap/tests/sub_pybind/relative_wrapped_import_lib.py
+++
@@ -1,1 +0,0 @@
-from .nested_wrapped_pybind
