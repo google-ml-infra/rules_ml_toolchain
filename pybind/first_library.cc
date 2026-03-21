01d87cbd1da6d3cbbbe912e241efdc664e65cfe0
Yurii T. <ytopin@griddynamics.com>
2026-03-21 05:35:02 +0000

    Add draft version of python tests

---
+++ pybind/first_library.cc
@@ -0,0 +1,6 @@
+#include "first_library.h"
+#include "second_library.h"
+
+int first_func(int x) {
+  return second_func(x) +  1;
+}
---
+++ pybind/py_common_library_test.cc
@@ -0,0 +1,48 @@
+#include "fifth_library.h"
+#include "first_library.h"
+#include "second_library.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "gtest/gtest.h"
+
+std::string read_file(const std::string& filename) {
+  std::ifstream file(filename);
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  return buffer.str();
+}
+
+TEST(PyCommonLibraryTest, PyCommonLibraryTest) {
+  std::cout << "1: first_func" << std::endl;
+  EXPECT_EQ(first_func(1), 2);
+  std::cout << "2: second_func" << std::endl;
+  EXPECT_EQ(second_func(1), 2);
+  std::cout << "4: first_func" << std::endl;
+  EXPECT_EQ(first_func(1), 4);
+  std::cout << "5: second_func" << std::endl;
+  EXPECT_EQ(second_func(1), 4);
+  std::cout << "7: second_global_func" << std::endl;
+  EXPECT_EQ(second_global_func(), 2);
+  std::cout << "8: second_global_func" << std::endl;
+  EXPECT_EQ(second_global_func(), 2);
+  std::cout << "9: fifth_func" << std::endl;
+  EXPECT_EQ(fifth_func(), 2);
+
+  std::cout << "10: binary resource size" << std::endl;
+#ifdef _WIN32
+  EXPECT_TRUE(!read_file("data/data_binary.exe").empty());
+#else
+  EXPECT_TRUE(!read_file("data/data_binary").empty());
+#endif // _WIN32
+  std::cout << "11: data/static_resource" << std::endl;
+  EXPECT_EQ(read_file("data/static_resource.txt"),
+            "A static resource file under data dir");
+  std::cout << "12: pybind/static_resource.txt" << std::endl;
+  EXPECT_EQ(read_file("pybind/static_resource.txt"),
+            "A static resource file under pybind dir");
+
+}
+
