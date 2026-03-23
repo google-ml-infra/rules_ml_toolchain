abce05ad7934f65f0d6bb5ec24d44777bd71a0ea
Yurii T. <ytopin@griddynamics.com>
2026-03-23 17:57:29 +0000

    Add --test_output=errors flag

--- py/rules_pywrap/tests/pybind_with_starlark_only_py_test.py
+++ py/rules_pywrap/tests/pybind_with_starlark_only_py_test.py
@@ -1,8 +1,21 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import unittest
 from py.rules_pywrap.tests import pybind as regular
 from py.rules_pywrap.tests import pybind_copy as regular_copy
 from py.rules_pywrap.tests import pybind_with_starlark_only as regular_with_starlark_only
-
 
 class PybindTest(unittest.TestCase):
   def test_pybind_first(self):
