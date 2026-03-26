2185435d9cb0a5fb7a963c2c7895201484135cee
Yurii T. <ytopin@griddynamics.com>
2026-03-26 22:24:23 +0000

    Remove unused directories

--- pybind/regular_copy.h
+++
@@ -1,7 +0,0 @@
-#ifndef _REGULAR_COPY_H_
-#define _REGULAR_COPY_H_
-
-int regular_copy_first_func(int x);
-int regular_copy_second_func(int x);
-
-#endif  // _REGULAR_H_
--- pybind/third_library.h
+++
@@ -1,6 +0,0 @@
-#ifndef _THIRD_LIBRARY_H_
-#define _THIRD_LIBRARY_H_
-
-int third_func(int x);
-
-#endif  // _THIRD_LIBRARY_H_
