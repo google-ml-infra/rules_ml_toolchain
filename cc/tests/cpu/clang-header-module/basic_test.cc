8606d86e474fa1cc3f1a5f126dc9282115b2fe3b
Yurii T. <ytopin@griddynamics.com>
2026-08-12 01:25:38 +0000

    Clang header modules support

---
+++ cc/tests/cpu/clang-header-module/basic_test.cc
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "utils.h"
+
+#if !defined(__clang__) || !__has_feature(modules)
+#error "Clang header modules are not enabled in Bazel!"
+#endif
+
+TEST(MathTest, AddsPositiveNumbers) {
+    EXPECT_EQ(add(2, 2), 4);
+}
